f8a84587e8eddf0eb68d665063eca18446753368
ankuranurag2 <ankuranurag2@gmail.com>
2020-09-01 20:13:16 +0530

    added Selection Sort

---
+++ src/sorting/SelectionSort.kt
@@ -0,0 +1,24 @@
+package sorting
+
+/**
+ * Created by ankur on 01/09/20
+ */
+
+fun main() {
+    val intArray = intArrayOf(10, 15, 12, 5, 2, 3, 99)
+
+    for (i in intArray.indices) {
+        var minIndex = i
+        for (j in i until intArray.size)
+            if (intArray[j] < intArray[minIndex])
+                minIndex = j
+        
+        if (i != minIndex) {
+            val temp = intArray[minIndex]
+            intArray[minIndex] = intArray[i]
+            intArray[i] = temp
+        }
+    }
+
+    println("After sorting : ${intArray.joinToString()}")
+}
